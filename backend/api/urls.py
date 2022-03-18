7d7b3b709566ea6cfdead3d2b7dca726323a083d
nichaos2 <nick.ioanou86@gmail.com>
2022-03-18 18:25:06 +0200

    black

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 
 from . import views
+
 # from .views import api_home
 
 
-urlpatterns = [
-    path('', views.api_home) # localhost:8000/api/
-]
+urlpatterns = [path("", views.api_home)]  # localhost:8000/api/
