218070058f6749d51cce733ee17ef426206bc611
nichaos2 <nick.ioanou86@gmail.com>
2022-03-18 18:24:43 +0200

    first api view

--- py_client/basic.py
+++ py_client/basic.py
@@ -2,7 +2,7 @@ import requests
 
 # endpoint = "https://httpbin.org/status/200/"
 # endpoint = "https://httpbin.org/anything"
-endpoint = "http://127.0.0.1:8000/"
+endpoint = "http://127.0.0.1:8000/api"
 
 get_response = requests.get(endpoint, json={"query": "Hello world"}) # HTTP Request
 print(get_response.text) # print raw text response
@@ -11,5 +11,5 @@ print(get_response.text) # print raw text response
 # HTTP Request -> HTML
 # REST API HTTP Request -> JSON
 # JavaScript Object Notation ~ Python Dict
-print(get_response.json())
 print(get_response.status_code)
+print(get_response.json())
