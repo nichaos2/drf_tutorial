bca058c4376a822823ace975de33ad0a44cd2298
nichaos2 <nick.ioanou86@gmail.com>
2022-03-18 19:00:57 +0200

    create first return - tedious serialization

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,21 +1,14 @@
 import json
 from django.http import JsonResponse
 
+from products.models import Product
+
 def api_home(request, *args, **kwargs):
-    # request -> HttpRequest -> Django
-    # print(dir(request))
-    # request.body
-    print(f'GET params : {request.GET}') # url query params
-    print(f'POST params : {request.POST}')
-    body = request.body # byte string of JSON data
+    model_data = Product.objects.all().order_by("?").first()
     data = {}
-    try:
-        data = json.loads(body) # string of JSON data -> Python Dict
-    except:
-        pass
-    print(f'data json : {data}')
-    # data['headers'] = request.headers # request.META ->
-    data['params'] = dict(request.GET)
-    data['headers'] = dict(request.headers)
-    data['content_type'] = request.content_type
+    # tedious way for serialization
+    if model_data:
+        data['title'] = model_data.title
+        data['content'] = model_data.content
+        data['price'] = model_data.price
     return JsonResponse(data)
