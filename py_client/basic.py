eeebfa243a84575573239e14e819797407c3940f
nichaos2 <nick.ioanou86@gmail.com>
2022-03-18 18:15:34 +0200

    change endpoint to django app

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,7 +1,8 @@
 import requests
 
 # endpoint = "https://httpbin.org/status/200/"
-endpoint = "https://httpbin.org/anything"
+# endpoint = "https://httpbin.org/anything"
+endpoint = "http://127.0.0.1:8000/"
 
 get_response = requests.get(endpoint, json={"query": "Hello world"}) # HTTP Request
 print(get_response.text) # print raw text response
